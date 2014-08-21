e19ebe0a4087c6cda5cca6cd651a618afa89d5f6
Greg Beech <greg@blinkbox.com>
2014-08-21 13:52:33 +0100

    Added support for serializing LocalDate instances
    ### New features
    
    - Now serialises joda `LocalDate` instances in the format `yyyy-MM-dd`
    so these can be used when a date with no time is needed. This is also
    more semantically correct as a LocalDate “represents a date without a
    time or time zone”.

--- src/test/scala/com/blinkbox/books/json/DefaultFormatsTests.scala
+++ src/test/scala/com/blinkbox/books/json/DefaultFormatsTests.scala
@@ -1,14 +1,16 @@
 package com.blinkbox.books.json
 
-import java.net.{URL, URI}
+import java.net.{URI, URL}
 
-import org.joda.time.{DateTimeZone, DateTime}
+import org.joda.time.chrono.ISOChronology
+import org.joda.time.{DateTime, DateTimeZone, LocalDate}
 import org.json4s.MappingException
 import org.json4s.jackson.Serialization.{read, write}
 import org.scalatest.FunSuite
 
 object DefaultFormatsTests {
   case class ObjectWithDateTime(value: DateTime)
+  case class ObjectWithLocalDate(value: LocalDate)
   case class ObjectWithURI(value: URI)
   case class ObjectWithURL(value: URL)
 }
@@ -58,6 +60,26 @@ class DefaultFormatsTests extends FunSuite {
   test("Does not deserialize non-ISO DateTime formats") {
     intercept[MappingException] { read[ObjectWithDateTime]("""{"value":"Fri, 09 Sep 2005 13:51:39 -0700"}""") }
     intercept[MappingException] { read[ObjectWithDateTime]("""{"value":"9/9/2005 1:51:39 PM"}""") }
+  }
+
+  test("Serializes and deserializes a UTC LocalDate") {
+    val obj = ObjectWithLocalDate(new LocalDate(2014, 7, 12))
+    val json = write(obj)
+    assert(json == """{"value":"2014-07-12"}""")
+    assert(obj == read[ObjectWithLocalDate](json))
+  }
+
+  test("Serializes and deserializes a non-UTC LocalDate, ignoring any time zone") {
+    val obj = ObjectWithLocalDate(new LocalDate(2014, 7, 12, ISOChronology.getInstance(DateTimeZone.forOffsetHours(3))))
+    val json = write(obj)
+    assert(json == """{"value":"2014-07-12"}""")
+    assert(obj == read[ObjectWithLocalDate](json)) // equality of LocalDate ignores the time zone anyway
+  }
+
+  test("Does not deserialize non-ISO LocalDate formats") {
+    intercept[MappingException] { read[ObjectWithLocalDate]("""{"value":"12-07-2014"}""") }
+    intercept[MappingException] { read[ObjectWithLocalDate]("""{"value":"12/07/2014"}""") }
+    intercept[MappingException] { read[ObjectWithLocalDate]("""{"value":"12 Jul 2014"}""") }
   }
 
   test("Serializes and deserializes objects with absolute URIs") {
