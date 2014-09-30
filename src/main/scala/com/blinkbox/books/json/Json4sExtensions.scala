62ef002517d60d8417bc33d38fe25e95a4fc4fe7
Anargyros Akrivos <anargyros@blinkbox.com>
2014-09-30 11:03:17 +0100

    Moved MessageDigest inside val since it is not thread safe

--- src/main/scala/com/blinkbox/books/json/Json4sExtensions.scala
+++ src/main/scala/com/blinkbox/books/json/Json4sExtensions.scala
@@ -7,13 +7,16 @@ import org.json4s.jackson.JsonMethods
 import scala.language.implicitConversions
 
 object Json4sExtensions {
-  private val md = java.security.MessageDigest.getInstance("SHA-1")
   implicit class RichJValue(val v: JValue) extends JsonMethods {
     def removeDirectField(key: String): JValue = v.remove(_ == v \ key)
     def overwriteDirectField(key: String, value: JValue) = {
       val newKey: JValue = key -> value
       removeDirectField(key).merge(newKey)
     }
-    lazy val sha1 = md.digest(compact(v).getBytes("UTF-8")).map("%02x".format(_)).mkString
+    lazy val sha1: String = {
+      // MessageDigest is not thread safe
+      val md = java.security.MessageDigest.getInstance("SHA-1")
+      md.digest(compact(v).getBytes("UTF-8")).map("%02x".format(_)).mkString
+    }
   }
 }
