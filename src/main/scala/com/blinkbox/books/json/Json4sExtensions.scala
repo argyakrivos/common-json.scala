f28ecac0dbb3d8c158e27f859e996323860330cb
Anargyros Akrivos <anargyros@blinkbox.com>
2014-09-30 10:52:12 +0100

    Added Json4sExtensions

---
+++ src/main/scala/com/blinkbox/books/json/Json4sExtensions.scala
@@ -0,0 +1,19 @@
+package com.blinkbox.books.json
+
+import org.json4s.JsonDSL._
+import org.json4s._
+import org.json4s.jackson.JsonMethods
+
+import scala.language.implicitConversions
+
+object Json4sExtensions {
+  private val md = java.security.MessageDigest.getInstance("SHA-1")
+  implicit class RichJValue(val v: JValue) extends JsonMethods {
+    def removeDirectField(key: String): JValue = v.remove(_ == v \ key)
+    def overwriteDirectField(key: String, value: JValue) = {
+      val newKey: JValue = key -> value
+      removeDirectField(key).merge(newKey)
+    }
+    lazy val sha1 = md.digest(compact(v).getBytes("UTF-8")).map("%02x".format(_)).mkString
+  }
+}
